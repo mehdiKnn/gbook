238db9284e2812ccae0ecffabcf0c2c87d23072a
Mehdi Kannouni <mehdi.kannouni@hotmail.fr>
2020-04-05 17:59:30 +0200

    V1 gbook pwa

---
+++ src/components/Form.vue
@@ -0,0 +1,16 @@
+<template>
+  <ion-item>
+    <ion-label position="floating">Search books</ion-label>
+    <ion-input></ion-input>
+  </ion-item>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
